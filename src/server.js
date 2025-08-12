a6c07c46299f433a10445b6f4d661f6eff561db8
Long Nguyen <longwinpro113@gmail.com>
2025-08-12 23:26:27 +0700

    create enviroment variables (env), connection with mongoDB

--- src/server.js
+++ src/server.js
@@ -1,14 +1,36 @@
-const express = require('express')
+import express from 'express'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/enviroment'
 
-const app = express()
+import { APIs_V1 } from '~/routes/v1'
 
-const hostname = 'localhost'
-const port = 3000
+const START_SERVER = () => {
+  const app = express()
+  const hostname = 'localhost'
+  const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello long</h1>')
-})
+  app.use('/v1', APIs_V1) 
 
-app.listen(port, hostname, () => {
-    console.log(`Hello LongNguyen. Server running at http://${hostname}:${port}/`)
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`3. Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`)
+  })
+
+  exitHook(() => {
+    console.log('4. Server is shutting down...')
+    CLOSE_DB()
+    console.log('5. Disconnected from Mongo Cloud Atlas')
+  })
+}
+
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB Cloud Atlas!')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB Atlas!')
+    START_SERVER()
+  } catch (error) {
+    // console.error(error)
+    process.exit(0)
+  }
+})()
